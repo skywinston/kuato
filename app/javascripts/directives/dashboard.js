513633fa8560ed841cbfd5ef207fecb182666e06
Sky Winston <skywinston@gmail.com>
2016-01-02 13:59:15 -0700

    select all now works, correctly adding/removing deck ids into the study queue.  still need to make the label for select all trigger the select all as well

--- app/javascripts/directives/dashboard.js
+++ app/javascripts/directives/dashboard.js
@@ -20,17 +20,46 @@ angular.module('kuato')
 
                 var queue = $scope.queuedForStudy;
 
-                if (queue.indexOf(id) > -1) {
+                if (queue.indexOf(id) === -1) {
                     queue.push(id);
+                    console.log('Adding deck ' + id + ' to the queue: ', $scope.queuedForStudy);
                 } else {
                     queue.splice(queue.indexOf(id), 1);
+                    console.log('Removing deck ' + id + ' from the queue: ', $scope.queuedForStudy);
                 }
 
+            };
+
+            $scope.selectAll = function (event) {
+                console.log(event.target);
+                $elem = $(event.target);
+                $unselected = $('.deck__checkbox--unselected');
+
+                // Toggle class on the select all box;
+                $elem.toggleClass('dashnav__selectall--unselected dashnav__selectall--selected');
+
+                // If the select all is selected, empty the queued for study array and fill it with every deck id,
+                // toggling classes on each checkbox.
+                if ($elem.hasClass('dashnav__selectall--selected')) {
+                    $unselected.addClass('deck__checkbox--selected').removeClass('deck__checkbox--unselected');
+                    $scope.queuedForStudy = [];
+                    $scope.decks.forEach(function (deck) {
+                        $scope.queuedForStudy.push(deck.id);
+                    });
+                    console.log("Adding all decks to the queue for study: ", $scope.queuedForStudy);
+
+                // But if select all box is unselected, remove the selected class from checkboxes and empty the queue
+                } else {
+                    $selected = $('.deck__checkbox--selected');
+                    $selected.removeClass('deck__checkbox--selected').addClass('deck__checkbox--unselected');
+                    $scope.queuedForStudy = [];
+                    console.log("Removing all decks from the queue: ", $scope.queuedForStudy);
+                }
             }
-
         },
-        link: function (scope, element, attrs) {
 
+        link: function (scope, elem, attrs) {
+            // todo — get a handle on the select all label span and when clicked, make it trigger a click on select all box
         }
     };
 
